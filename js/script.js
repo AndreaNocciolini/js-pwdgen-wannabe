d856ac12fb2cd16ff250badac88f9e6731e3724d
AndreaNocciolini <andreanocciolini93@gmail.com>
2021-11-18 14:15:49 +0100

    add-prompts

--- js/script.js
+++ js/script.js
@@ -2,3 +2,12 @@
 // poi chiedi il suo cognome,
 // poi chiedi il suo colore preferito
 // Infine scrivi sulla pagina nomecognomecolorepreferito21
+
+const  name=prompt(`What's your name?`);
+console.log(`name`);
+
+const surname=prompt(`What's your surname?`);
+console.log(`surname`)
+
+const favouriteColor=prompt(`And your favourite color is?`)
+console.log(`favouriteColor`)
